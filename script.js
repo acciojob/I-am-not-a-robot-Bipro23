cdb27769cec62afbe32007424106ba398f20d3c2
Bipro23 <88981554+Bipro23@users.noreply.github.com>
2023-09-28 20:18:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,99 +1,51 @@
 //your code here
-//your JS code here. If required.
-function shuffleArray(array) {
-      for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-      }
-      return array;
-    }
+min =1;
+max =5;
+var num = Math.floor(Math.random() * (max - min + 1)) + min;
+const verifyBtn = document.querySelector("#verify");
+const resetBtn = document.querySelector("#reset")
+const para = document.getElementById("para");
+var clicked=0;
+var selected=0;
+var imgName = "img"+num;
+const container = document.getElementsByClassName("flex")[0];
+const box = document.createElement("div");
+box.classList.add("img");
+box.classList.add(`${imgName}`);
+container.appendChild(box);
 
-    // Helper function to check if two images have the same class
-    function isIdentical(img1, img2) {
-      return img1.className === img2.className;
-    }
+box.addEventListener('click',()=>{
+	box.classList.add("selected");
+	selected = num;
+	console.log(num);
+})
 
-    // Get the image elements
-    const imageContainer = document.getElementById("image-container");
-    const images = imageContainer.getElementsByTagName("img");
-  
-    // Create an array of class names for the images
-    const classNames = ["img1", "img2", "img3", "img4", "img5"];
-  
-    // Shuffle the class names and assign them to the images
-    const shuffledClassNames = shuffleArray([...classNames, ...classNames]);
-    for (let i = 0; i < images.length; i++) {
-      images[i].className = shuffledClassNames[i];
-    }
-  
-    // Store the selected images
-    let selectedImages = [];
-  
-    // Store the current state
-    let state = 1;
-  
-    // Function to handle image click
-    function handleImageClick(event) {
-      const clickedImage = event.target;
-  
-      // Ignore clicks on already selected images
-      if (selectedImages.includes(clickedImage)) {
-        return;
-      }
-  
-      // Reset the previous selection if more than two images are selected
-      if (selectedImages.length >= 2) {
-        selectedImages.forEach((image) => image.classList.remove("selected"));
-        selectedImages = [];
-      }
-  
-      // Add the clicked image to the selection
-      clickedImage.classList.add("selected");
-      selectedImages.push(clickedImage);
-  
-      // Change state based on the number of selected images
-      if (selectedImages.length === 1) {
-        state = 2;
-        document.getElementById("reset").style.display = "block";
-      } else if (selectedImages.length === 2) {
-        state = 3;
-        document.getElementById("verify").style.display = "block";
-      }
-    }
-  
-    // Function to handle reset button click
-    function handleResetClick() {
-      selectedImages.forEach((image) => image.classList.remove("selected"));
-      selectedImages = [];
-  
-      state = 1;
-      document.getElementById("reset").style.display = "none";
-      document.getElementById("verify").style.display = "none";
-      document.getElementById("para").innerHTML = "";
-    }
-  
-    // Function to handle verify button click
-    function handleVerifyClick() {
-      const img1 = selectedImages[0];
-      const img2 = selectedImages[1];
-  
-      if (isIdentical(img1, img2)) {
-        document.getElementById("para").innerHTML = "You are a human. Congratulations!";
-      } else {
-        document.getElementById("para").innerHTML = "We can't verify you as a human. You selected the non-identical tiles.";
-      }
-  
-      state = 4;
-      document.getElementById("verify").style.display = "none";
-    }
-  
-    // Attach click event listeners to the images
-    for (let i = 0; i < images.length; i++) {
-      images[i].addEventListener("click", handleImageClick);
-    }
-  
-    // Attach click event listener to the reset button
-    document.getElementById("reset").addEventListener("click", handleResetClick);
-  
-    // Attach click event listener to the verify button
-    document.getElementById("verify").addEventListener("click", handleVerifyClick);
+var isSelected=false;
+var select = document.getElementsByClassName("img");
+for(let i=0;i<select.length-1;i++){
+	select[i].addEventListener('click',()=>{
+		if(isSelected == false){
+		select[i].classList.add("selected");
+		isSelected = true;
+		clicked = (i+1);
+		console.log(i+1);
+		}
+	})
+}
+
+verifyBtn.addEventListener('click',()=>{
+	if(isSelected === true){
+		verifyBtn.style.display = "none";
+	if(selected === clicked && isSelected === true){
+		para.innerHTML += "You are a human. Congratulations!.";
+	}else{
+		para.innerHTML += "We can't verify you as a human. You selected the non-identical tiles.";
+	}
+	}else{
+		alert("Please Selecte the Images")
+	}
+})
+
+resetBtn.addEventListener('click',()=>{
+		location.reload();
+})
